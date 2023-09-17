def125bbbe2abd754ab81aaf82f07a1efff44407
Jorgito Ramírez Galvan <jorgitoram@Jorgitos-MacBook-Air.local>
2023-09-17 21:14:19 +0200

    envirioment set up

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from requests.auth import HTTPBasicAuth
 import pprint
 from dotenv import load_dotenv
 import os
+from http.client import HTTPSConnection
+from base64 import b64encode
 
 load_dotenv()
 
@@ -13,15 +15,27 @@ PUB_KEY = os.environ.get('PUB_KEY')
 URL = 'https://learn.robosats.com/'
 CREATE_USER_ENDPOINT = "api/user/"
 TOKEN = os.environ.get('TOKEN')
-USER = 'FunnyTitanium24'
 
-params = {
+params2 = {
   "token_sha256": TOKEN_SHA256,
   "public_key": PUB_KEY,
   "encrypted_private_key": PRIV_KEY,
 }
 
-response = requests.get(url=URL+CREATE_USER_ENDPOINT, auth=HTTPBasicAuth(USER, TOKEN), params=params)
+params = [
+  {
+    "id": 0,
+    "expires_at": "2019-08-24T14:15:22Z",
+    "type": 0,
+    "currency": 0,
+  }
+]
+
+def basic_auth(username, password):
+    token = b64encode(f"{username}:{password}".encode('utf-8')).decode("ascii")
+    return f'Basic {token}'
+
+response = requests.get(url=URL+CREATE_USER_ENDPOINT, params=params)
 
 """ if response.status_code == 200:
     # Request was successful
